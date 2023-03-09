30a7039ed37e070f5684c733247a9018edfe07ca
vegaSeB <sebasquero2005@gmail.com>
2023-03-08 22:23:06 -0500

    cambio del cliente

--- ClientePokemon/src/co/edu/unbosque/model/Cliente.java
+++ ClientePokemon/src/co/edu/unbosque/model/Cliente.java
@@ -41,170 +41,42 @@ public class Cliente extends Thread{
 		}
 	}
 	
-	public void enviarCajas(String caja, ArrayList<JPanel> paneles) {
+public Object recibir(String cedula) {
+		
+		Object salida = true;
 		
 		try {
-			cliente = new Socket(address,port);
+			cliente = new Socket(address, port);
 			enviar = new ObjectOutputStream(cliente.getOutputStream());
-			enviar.writeObject(caja);
-			System.out.println("Se evio la caja");
+			enviar.writeObject(cedula);
+
 			
 			servidor = new ServerSocket(port+1);
 			cliente = servidor.accept();
 			recibir = new ObjectInputStream(cliente.getInputStream());
-			
-			String res = (String)recibir.readObject();
-			
-			if(res.equalsIgnoreCase("caja1")) {
-				
-			enviar.writeObject(paneles);
-			System.out.println("Se evio la caja en paneles");
-			enviar.close();
-			cliente.close();
-			recibir.close();
-			
-			}else if(res.equalsIgnoreCase("caja2")) {
-				
-				enviar.writeObject(paneles);
-				enviar.close();
-				cliente.close();
-				recibir.close();
-				
-			}else if(res.equalsIgnoreCase("caja3")) {
-				
-				enviar.writeObject(paneles);
-				enviar.close();
-				cliente.close();
-				recibir.close();
-				
-			}else {
-				
-				enviar.close();
-				cliente.close();
-				recibir.close();
-				
-			}
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-		} 
-		
-	}
-	
-	public Object recibirCajas(String caja) {
-		
-		Object recibido = null;
-		
-		try {
-			
-			cliente = new Socket(address,port);
-			enviar = new ObjectOutputStream(cliente.getOutputStream());
-			enviar.writeObject(caja);
-			
-			servidor = new ServerSocket(port+1);
-			cliente = servidor.accept();
-			recibir = new ObjectInputStream(cliente.getInputStream());
-			ArrayList<JPanel> res = (ArrayList<JPanel>) recibir.readObject();
-			
-			if (res == null) {
+			String aux = (String) recibir.readObject();
+			if (aux == null) {
 				cliente.close();
 				enviar.close();
 				servidor.close();
 				recibir.close();
-				recibido = null;
+				salida = null;
 			} else {
 				cliente.close();
 				enviar.close();
 				servidor.close();
 				recibir.close();
-				recibido = res;
+				salida =  aux;
 			}
 			
-		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return recibido;
-		
-	}
-	
-	public void enviarBolsillo(JPanel[] bolsillo) {
-		
-		try {
 			
-			cliente = new Socket(address,port);
-			enviar = new ObjectOutputStream(cliente.getOutputStream());
-			enviar.writeObject("bolsillo");
 			
-			servidor = new ServerSocket(port+1);
-			cliente = servidor.accept();
-			recibir = new ObjectInputStream(cliente.getInputStream());
-			
-			String res = (String)recibir.readObject();
-			if(res.equalsIgnoreCase("bolsillo")){
-				
-				cliente = new Socket(address, port);
-				enviar = new ObjectOutputStream(cliente.getOutputStream());
-				enviar.writeObject(bolsillo);
-				
-				cliente.close();
-				enviar.close();
-				servidor.close();
-				recibir.close();
-			}else {
-				
-				cliente.close();
-				enviar.close();
-				servidor.close();
-				recibir.close();
-				
-			}
-
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		
-	}
-	
-	public Object recibirBolsillo() {
 		
-		Object recibido = null;
-		
-		try {
-			
-			cliente = new Socket(address,port);
-			enviar = new ObjectOutputStream(cliente.getOutputStream());
-			enviar.writeObject("Bolsillo");
-			
-			servidor = new ServerSocket(port+1);
-			cliente = servidor.accept();
-			recibir = new ObjectInputStream(cliente.getInputStream());
-			JPanel[] res = (JPanel[]) recibir.readObject();
-			
-			if (res == null) {
-				cliente.close();
-				enviar.close();
-				servidor.close();
-				recibir.close();
-				recibido = null;
-			} else {
-				cliente.close();
-				enviar.close();
-				servidor.close();
-				recibir.close();
-				recibido = res;
-			}
-			
-		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return recibido;
-		
+		return salida;
 	}
 	
 }
